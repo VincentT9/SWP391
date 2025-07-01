9a4e9875a0e377f87193b29ff9bba0b0f61b8020
bread3r <khanh.luan.1603@gmail.com>
2025-07-01 15:27:33 +0700

    Fix bug admin page

--- src/components/promo/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MedicationPromoPage } from './MedicationPromoPage';
-export { default as VaccinationPromoPage } from './VaccinationPromoPage';
-export { default as HealthCheckPromoPage } from './HealthCheckPromoPage';
